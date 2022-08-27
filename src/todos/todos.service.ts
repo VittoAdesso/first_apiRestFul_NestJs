83bf79193631215acdd3b77ac10809c42acf511e
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-27 18:24:40 +0200

    confg service

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './todo.interface';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateItemDto } from './dto/create-todo';
 
 @Injectable()
-export class TodosService {}
+
+export class ItemsService {
+    constructor(@InjectModel('Todo') private readonly todoModel: Model) {}
+    async findAll(): Promise<Todo[]> {
+      return await this.todoModel.find();
+    }
+    async findOne(id: string): Promise {
+      return await this.todoModel.findOne({ _id: id });
+    }
+    async create(item: CreateItemDto): Promise {
+      const newTodo = new this.todoModel(item);
+      return await newTodo.save();
+    }
+    async delete(id: string): Promise {
+      return await this.todoModel.findByIdAndRemove(id);
+    }
+    async update(id: string, todo: CreateTodoDto): Promise {
+      return await this.todoModel.findByIdAndUpdate(id, todo, { new: true });
+    }
+  }
